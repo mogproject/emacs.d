9413bbd4dbe943524ac1a71e85fb8f2a23ba9bec
Yosuke Mizutani <mogproj@gmail.com>
2016-11-19 00:19:21 -0500

    added clipboard access

--- init.el
+++ init.el
@@ -5,6 +5,13 @@
 ;;----------------------------------------------------------------------------
 ;; Check version
 ;;----------------------------------------------------------------------------
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (let ((minver "23.3"))
   (when (version<= emacs-version "23.1")
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
@@ -40,9 +47,24 @@
 ;;----------------------------------------------------------------------------
 (setq js-indent-level 2)
 
-
 ;;----------------------------------------------------------------------------
 ;; Disable electric-indent-mode
 ;;----------------------------------------------------------------------------
 (electric-indent-mode 0)
+
+;;----------------------------------------------------------------------------
+;; Clipboard access
+;;----------------------------------------------------------------------------
+
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+        (process-send-string proc text)
+        (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
 
