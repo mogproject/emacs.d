0bfb8817d691c461f5de5fec9e41c2159efe1957
Yosuke Mizutani <mogproj@gmail.com>
2017-08-23 09:43:19 -0400

    updated

--- lisp/init-java.el
+++ lisp/init-java.el
@@ -3,7 +3,26 @@
 ;;----------------------------------------------------------------------------
 
 (require 'eclim)
-(global-eclim-mode)
+(require 'eclimd)
+
+;(global-eclim-mode)
+
+(add-hook 'java-mode-hook 'eclim-mode)
+
+;;----------------------------------------------------------------------------
+;; auto complete
+;;----------------------------------------------------------------------------
+;; regular auto-complete initialization
+(require 'auto-complete-config)
+(ac-config-default)
+;; add the emacs-eclim source
+(require 'ac-emacs-eclim)
+(ac-emacs-eclim-config)
+
+;; for company-mode
+;; (require 'company-emacs-eclim)
+;; (company-emacs-eclim-setup)
+
 
 ;(add-to-list 'ac-modes 'jde-mode)
 
